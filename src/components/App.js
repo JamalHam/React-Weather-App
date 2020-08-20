1bd2f405e8697be9a44147ecc2b1ecdac2c0b826
Jamal Hamilton <hamilton.jamal.anthony@gmail.com>
2020-08-20 10:39:35 -0400

    Starting App

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Main app section</p>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
